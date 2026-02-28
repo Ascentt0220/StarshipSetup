7c19caafe7667c2a598e7b8a32ae18d8991437d6
Ascentt0220 <cedar.benjamin@gmail.com>
2026-02-28 13:10:20 +0000

    fixed a few things

--- setup.ps1
+++ setup.ps1
@@ -2,29 +2,29 @@ $FontPath = Test-Path -Path "$env:USERPROFILE\Fonts" -ErrorAction SilentlyContin
 
 if ($FontPath -eq $false) {
     New-Item -Path "$env:USERPROFILE\Fonts" -Name "Fonts" -ItemType "Directory" -Force
-    Write-Host "Created Fonts directory at $env:USERPROFILE\Fonts"
+    Write-Host "Created Fonts directory at $env:USERPROFILE\Fonts" -ForegroundColor Green
 }
 
-$ChocoInstalled = Test-Path -Path $env:ChocolateyInstall -ErrorAction SilentlyContinue
+$ChocoInstalled = Test-Path -Path "$env:PROGRAMDATA\chocolatey" -ErrorAction SilentlyContinue
 
 if ($ChocoInstalled -eq $false) {
-    Write-Host "Installing Chocolatey..."
+    Write-Host "Installing Chocolatey..." -ForegroundColor Cyan
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    Write-Host "Chocolatey has been installed."
+    Write-Host "Chocolatey has been installed" -ForegroundColor Green
 }
 
 if ($ChocoInstalled) {
-    Write-Host "Installing FiraCode font"
+    Write-Host "Installing FiraCode font" -ForegroundColor Cyan
     choco install firacode -y
-    Write-Host "FiraCode font has been installed."
+    Write-Host "FiraCode font has been installed." -ForegroundColor Green
 }
 
 $FontInstalled = Get-ChildItem -Path "$env:USERPROFILE\Fonts" -Filter "FiraCode*" -ErrorAction SilentlyContinue
 
 if ($FontInstalled.Count -gt 0) {
-    Write-Host "Installing starship..."
+    Write-Host "Installing starship..." -ForegroundColor Cyan
     choco install starship -y
-    Write-Host "Starship has been installed."
+    Write-Host "Starship has been installed" -ForegroundColor Green
 }
 
 $StarshipConfig = "$env:USERPROFILE\.config\starship.toml"
@@ -35,4 +35,4 @@ if ($StarshipConfigExists -eq $false) {
     iwr -Uri "https://raw.githubusercontent.com/Ascentt0220/StarshipSetup/refs/heads/main/starship.toml" -OutFile "$env:USERPROFILE\.config\starship.toml"
 }
 
-Write-Host "Starship setup is complete. Please restart your terminal to apply the changes."
+Write-Host "Starship setup is complete. Please restart your terminal to apply the changes." -ForegroundColor Green
